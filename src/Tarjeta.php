d8cf43a53b198b267d8cc01818302cd806de787b
Alesandro Regolo <regoloalesandro@gmail.com>
2018-08-10 20:55:16 -0300

    inteto

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -6,12 +6,20 @@ class Tarjeta implements TarjetaInterface {
     protected $saldo;
 
     public function recargar($monto) {
-      // Esto esta hecho mal a proposito.
-      if ($monto % 2 == 0) {
-        $this->saldo += $monto;
-      }
+	  if($monto == 10 || $monto == 20 || $monto == 30 || $monto == 50 || $monto == 100 || $monto == 510,15 || $monto == 962,59)
+	  {
+			if(this->$saldo == 0,0){
+				this->$saldo= $monto;
+			}
+			else {
+	   			this->$saldo = this->$saldo + $monto;
+			}
+			return True;
+	  }
+	  else{
+	  	  return False;
+	  }
 
-      return $monto % 2 == 0;
     }
 
     /**
