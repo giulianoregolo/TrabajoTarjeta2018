ed6bafcfe4f3f530d1c042d34f71e675a48a2b1f
Alesandro Regolo <regoloalesandro@gmail.com>
2018-08-10 21:14:49 -0300

    ingresar monto

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -6,13 +6,13 @@ class Tarjeta implements TarjetaInterface {
     protected $saldo;
 
     public function recargar($monto) {
-	  if($monto == 10 or $monto == 20 or $monto == 30 or $monto == 50 or $monto == 100 or $monto == 510,15 or $monto == 962,59)
+	  if($monto == 10.0 || $monto == 20.0 || $monto == 30.0 || $monto == 50.0 || $monto == 100.0 || $monto == 510.15 || $monto == 962.59)
 	  {
-			if(this->$saldo == 0,0){
-				this->$saldo= $monto;
+			if($this->saldo == 0.0){
+				$this->saldo= $monto;
 			}
 			else {
-	   			this->$saldo = this->$saldo + $monto;
+	   		  $this->saldo = $this->saldo + $monto;
 			}
 			return True;
 	  }
