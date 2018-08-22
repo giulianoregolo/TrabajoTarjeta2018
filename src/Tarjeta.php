82e0868add95b0ba0ee612d49248a2011b06d0fe
Giuliano Regolo <gripdesexto6e@gmail.com>
2018-08-21 21:29:17 -0300

    recarga conplus

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -4,22 +4,24 @@ namespace TrabajoTarjeta;
 
 class Tarjeta implements TarjetaInterface {
     protected $saldo;
-
+	protected $viajesplus = 0;
     public function recargar($monto) {
 	  if($monto == 10.0 || $monto == 20.0 || $monto == 30.0 || $monto == 50.0 || $monto == 100.0 || $monto == 510.15 || $monto == 962.59)
 	  {
-			if($this->saldo == 0.0){
-				$this->saldo= $monto;
-			}
-			else {
-	   		  $this->saldo = $this->saldo + $monto;
-			}
-			return True;
+		if($this->saldo == 0.0){
+			$this->saldo= $monto;
+		}
+		else {
+	   		$this->saldo = $this->saldo + $monto;
+		}
+		if($this->saldo > 0.0 || $monto > 10.0){
+			$this->viajesplus = 0;
+		} 
+		return True;	    
 	  }
 	  else{
 	  	  return False;
 	  }
-
     }
 
     /**
