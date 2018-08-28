f6de2ff9047739e873f78860c4f9f7fffd92ff69
Giuliano Regolo <gripdesexto6e@gmail.com>
2018-08-28 01:37:48 -0300

    correciones de los test

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -4,7 +4,7 @@ namespace TrabajoTarjeta;
 
 class Tarjeta implements TarjetaInterface {
     public $saldo;
-    protected $viajesplus = 2;
+    public $viajesplus = 2;
     public $tipo=0;
     public function recargar($monto) {
 	  if($monto == 10.0 || $monto == 20.0 || $monto == 30.0 || $monto == 50.0 || $monto == 100.0 || $monto == 510.15 || $monto == 962.59)
@@ -15,6 +15,7 @@ class Tarjeta implements TarjetaInterface {
 		else {
 	   		$this->saldo = $this->saldo + $monto;
 		}
+
 		return True;	    
 	  }
 	  else{
--- tests/PagarTest.php
+++ tests/PagarTest.php
@@ -12,7 +12,7 @@ class PagarTest extends TestCase {
     public function testpagarmontoConsaldo() {
         $tarjeta = new Tarjeta;
         $colectivo = new Colectivo(NULL,NULL,NULL);
-        $boleto= new Boleto($colectivo,$tarjeta,$valor);
+        $boleto= new Boleto($colectivo,$tarjeta,$valor=14.80);
         $tarjeta->saldo = 14.80;
         
         $this->assertEquals($colectivo->pagarCon( $tarjeta), $boleto);
@@ -26,7 +26,7 @@ class PagarTest extends TestCase {
         $tarjeta = new Tarjeta;
 		$colectivo = new Colectivo(NULL,NULL,NULL);
 		$tarjeta->saldo = 0;
-        $boleto= new Boleto($colectivo,$tarjeta,$valor);
+        $boleto= new Boleto($colectivo,$tarjeta,$valor=14.80);
         $this->assertEquals($colectivo->pagarCon( $tarjeta), $boleto);
     }    
 }
