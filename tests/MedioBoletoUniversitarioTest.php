a4bf030afcbbc3480b951be062c40a6784eb8236
Nahuel Blando <nahuel.blando@gmail.com>
2018-09-23 20:53:39 -0300

    Agregado de paréntesis a new TarjetamedioBoletoUniversitario

--- tests/MedioBoletoUniversitarioTest.php
+++ tests/MedioBoletoUniversitarioTest.php
@@ -10,7 +10,7 @@ class MedioBoletoUniversitarioTest extends TestCase {
      * Comprueba que el medio boleto page la mitad que una tarjeta normal en un pago estandar.
      */
     public function testmedioboletouniversitarioTiempo() {
-        $medioboleto = new TarjetamedioBoletoUniversitario;
+        $medioboleto = new TarjetamedioBoletoUniversitario();
         $medioboleto->recargar(50.0);
         $medioboleto->pagarTarjeta();
         $medioboleto->pagarTarjeta();
