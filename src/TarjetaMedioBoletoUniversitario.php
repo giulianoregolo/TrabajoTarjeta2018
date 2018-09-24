6d95985071a1200707baacf07d26643dc570ca72
giulianoregolo <41971577+giulianoregolo@users.noreply.github.com>
2018-09-24 17:29:29 -0300

    Update TarjetaMedioBoletoUniversitario.php

--- src/TarjetaMedioBoletoUniversitario.php
+++ src/TarjetaMedioBoletoUniversitario.php
@@ -3,20 +3,18 @@
 namespace TrabajoTarjeta;
 
 class TarjetamedioBoletoUniversitario extends Tarjeta {
-    protected $valor = 7.40;
+    protected $valor = 14.80;
     protected $ultimopago;
     protected $cantidadpagos = 0;
     
-    public function _construct(){
-        $this->ultimopago = new DateTime();
+    public function obtenerValorBoleto(){
+        if ($this->medioDisponible()) return $this->valor/2;
+        else return $this->valor;
     }
-    
+
     public function pagarTarjeta(){
-        $tiempoactual = new DateTime();
-        if ($tiempoactual->getTimestamp() / 60 > $this->ultimopago->getTimestamp()){
-            $this->cantidadpagos = 0;
-        }
-        if($this->saldo < $this->valor){
+        $valoraux = obtenerValorBoleto();
+        if($this->saldo < $valoraux){
             switch($this->viajesplus){
                 case 0:
                     return false;
@@ -34,54 +32,84 @@ class TarjetamedioBoletoUniversitario extends Tarjeta {
         else{
             switch($this->viajesplus){
                 case 0:
-                    if ($this->cantidadpagos == 2){
-                            $this->valor = 14.80;
-                    }
-                    $this->valor= $this->valor+14.80+14.80;
-                    if($this->saldo < $this->valor){
+                    $valoraux= $valoraux+14.80+14.80;
+                    if($this->saldo < $valoraux){
                         return false;
                     }
                     else{
-                        $this->saldo = $this->saldo - $this->valor;
-                        $this->ultimopago = new DateTime();
-                        if ($this->cantidadpagos < 2){
+                        $this->saldo = $this->saldo - $valoraux;
+                            if ($this->cantidadpagos < 2){
                             $this->cantidadpagos = $this->cantidadpagos + 1;
                         }
-                        $this->ultimopago = new DateTime();
-                        return true;
+                        $this->ultimopago = $this->tiempo->time();
+                            return true;
                     }
 
                 case 1:
-                    if ($this->cantidadpagos == 2){
-                            $this->valor = 14.80;
-                    }
-                    $this->valor= $this->valor+14.80;
-                    if($this->saldo < $this->valor){
+                    $valoraux= $valoraux+14.80;
+                    if($this->saldo < $this->valoraux){
                         return false;
                     }
                     else{
-                        $this->saldo = $this->saldo - $this->valor;
-                        $this->ultimopago = new DateTime();
+                        $this->saldo = $this->saldo - $this->valoraux;
                         if ($this->cantidadpagos < 2){
                             $this->cantidadpagos = $this->cantidadpagos + 1;
                         }
-                        $this->ultimopago = new DateTime();
+                        $this->ultimopago = $this->tiempo->time();
                         return true;
                     }
                 
                 case 2:
-                    if ($this->cantidadpagos == 2){
-                            $this->valor = 14.80;
-                    }
-                    $this->saldo = $this->saldo - $this->valor;
-                    $this->ultimopago = new DateTime();
+                    $this->saldo = $this->saldo - $valoraux;
                     if ($this->cantidadpagos < 2){
                         $this->cantidadpagos = $this->cantidadpagos + 1;
                     }
-                    $this->ultimopago = new DateTime();
+                    $this->ultimopago = $this->tiempo->time();
                     return true;
                     break;
             }
+        }
+        public function tiempoDeEsperaCumplido(){
+            $ultimopago = $this->ultimopago);
+            $fecha_actual = $this->tiempo->time();
+            $diferencia_fechas = $fecha_actual - $ultimopago;
+            if($diferencia_fechas >= $this->obtenerTiempoDeEspera()){
+                    return TRUE;
+            }
+            return FALSE;
+        }
+
+        public function medioDisponible(){
+            if($this->cantidadpagos < 2){
+                return TRUE;
+            }
+            if($this->tiempoDeEsperaUltimoMedioCumplido()){
+              $this->cantidadpagos = 0;
+              return TRUE;
+            }
+            return FALSE;
+            
+        }
+
+        public function tiempoDeEsperaUltimoMedioCumplido(){
+            $fecha_ultima = $this->obtenerUltimaFechaPagada();
+            
+            $fecha_ultima = date("d/m/y", $fecha_ultima);
+        
+            $fecha_actual = $this->tiempo->time();
+            $fecha_actual = date("d/m/y", $fecha_actual);
+                
+            if($fecha_ultima < $fecha_actual){
+                 return TRUE;
+            }
+            return FALSE;
+     
+        }
+        public function obtenerTiempoDeEspera(){
+            return $this->tiempo_de_espera;
+        }
+        public function obtenerUltimaFechaPagada(){
+            return $this->ultimopago;
         }
     }
 }
