49cd1f874d2868a8f209edc2400208dcb0b96f23
Nahuel Blando <nahuel.blando@gmail.com>
2018-09-24 21:09:30 -0300

    Agregado de parentesis a la declaracion de clase

--- tests/BoletoTest.php
+++ tests/BoletoTest.php
@@ -7,7 +7,7 @@ use PHPUnit\Framework\TestCase;
 class BoletoTest extends TestCase {
 
     public function testSaldoCero() {
-        $tiempoprueba = new Tiempo;
+        $tiempoprueba = new Tiempo();
         $tarjeta = new Tarjeta($tiempoprueba, NULL);
         $colectivo = new Colectivo("mixta","103",420);
         $valor=14.80;
