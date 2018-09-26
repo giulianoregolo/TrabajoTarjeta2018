e38eca35d9711f9f84204be077dc4a4f149595a8
Nahuel Blando <nahuel.blando@gmail.com>
2018-09-25 23:32:40 -0300

    Prueba de funcionamiento de assert equals en testpagarmontoEstandar

--- tests/MedioBoletoTest.php
+++ tests/MedioBoletoTest.php
@@ -14,8 +14,8 @@ class MedioBoletoTest extends TestCase {
         $medioboleto = new TarjetamedioBoleto($tiempo, null);
         $colectivo = new Colectivo("mixta","133",420);
         $medioboleto->recargar(50.0);
-        $medioboleto->pagarTarjeta();
-        $this->assertEquals($medioboleto->obtenerSaldo(),(50.0-7.40));
+        $this->assertEquals($medioboleto->pagarTarjeta(),TRUE);
+        $this->assertEquals($medioboleto->obtenerSaldo(),(50.0-9.40));
     }
     
     /**
