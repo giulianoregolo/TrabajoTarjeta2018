5381bd70b55826ea13c277e78c45f57ced1c9da8
Nahuel Blando <nahuel.blando@gmail.com>
2018-11-25 16:13:54 -0300

    Estandar wordpress boleto test

---
+++ tests/boleto-test.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace TrabajoTarjeta;
+
+use PHPUnit\Framework\TestCase;
+
+class Boleto_Test extends TestCase {
+
+    public function test_saldo_cero() {
+        $tiempo_prueba = new Tiempo();
+        $tarjeta = new Tarjeta( $tiempoprueba, NULL );
+        $colectivo = new Colectivo( 'mixta','103', 420 );
+        $valor = 14.80;
+        $boleto = new Boleto( $colectivo, $tarjeta );
+        $this->assertEquals( $boleto->obtenerValor(), $valor );
+    }
+}
